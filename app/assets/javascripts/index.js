82a54d08ba4b7fb5e40b0e6db43cbc56a5833b9d
Dan McKeon <email": null>
2017-05-15 12:17:17 -0700

    add scrolling to top nav

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,5 +3,17 @@ $(document).ready(function() {
     e.preventDefault();
     $("#menu ul li").removeClass("current_page_item");
     $(this).addClass("current_page_item");
+    var targetAnchor = $(this).children().eq(0).attr("href");
+    var aTag = $("a[name='"+ targetAnchor +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+  });
+
+  $("#logo").on("click", "h1", function(e) {
+    e.preventDefault();
+    $("#menu ul li").removeClass("current_page_item");
+    $("#menu ul li").eq(0).addClass("current_page_item");
+    var targetAnchor = $(this).children("a").eq(0).attr("href");
+    var aTag = $("a[name='"+ targetAnchor +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
   });
 });
