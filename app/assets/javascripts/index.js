e06975ce052b5cdfdc549d6e06ff66f62ecc0152
Dan McKeon <email": null>
2017-05-15 11:47:09 -0700

    implement active nav element, window now needs to scroll

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  $("#menu").on("click", "li", function() {
+  $("#menu").on("click", "li", function(e) {
+    e.preventDefault();
     $("#menu ul li").removeClass("current_page_item");
     $(this).addClass("current_page_item");
   });
