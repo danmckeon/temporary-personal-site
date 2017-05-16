db93eb146b52378cb6f4a3ecd634dc581fdfa0b3
Dan McKeon <email": null>
2017-05-15 17:53:17 -0700

    add code for scroll fix - commented out currently

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -16,4 +16,41 @@ $(document).ready(function() {
     var aTag = $("a[name='"+ targetAnchor +"']");
     $('html,body').animate({scrollTop: aTag.offset().top},'slow');
   });
+
+  // Cache selectors
+  var topMenu = $("#header")
+  var topMenuHeight = topMenu.outerHeight()+15
+  // All list items
+  var menuItems = $("#header #menu").find("a")
+  // Anchors corresponding to menu items
+  var scrollItems = menuItems.map(function(){
+    var item = $($(this).attr("href"));
+    return item;
+  });
+
+  // $(window).on("scroll", function() {
+  //   console.log($(window).scrollTop());
+  //   // Get container scroll position
+  //   // var fromTop = $(this).scrollTop()+topMenuHeight;
+  //   // // Get id of current scroll item
+  //   // var cur = scrollItems.map(function(){
+  //   //   if ($(this).offset().top < fromTop)
+  //   //     return this;
+  //   // });
+
+  // });
+
+  // Bind to scroll
+  // $("#anchor").scroll(function(){
+  //   console.log("At anchor!")
+
+
+  //    // // Get the id of the current element
+  //    // cur = cur[cur.length-1];
+  //    // var id = cur && cur.length ? cur[0].id : "";
+  //    // // Set/remove active class
+  //    // menuItems
+  //    //   .parent().removeClass("current_page_item")
+  //    //   .end().filter("[href='#"+id+"']").parent().addClass("current_page_item");
+  // });​
 });
