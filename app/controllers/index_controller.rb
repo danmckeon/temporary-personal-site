103ef99eddcafa4f0818f8daedab411a13d0928c
Dan McKeon <email": null>
2017-05-17 10:11:37 -0700

    add route and controller acgtion for resume

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,10 @@
 class IndexController < ApplicationController
   def index
   end
+  def resume
+    p "it worked"
+    # respond_to do |format|
+    #   format.pdf
+    # end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'index#index'
+  get '/resume', to: 'index#resume'
 end
